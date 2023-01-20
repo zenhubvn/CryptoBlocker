5cc03baccd7dc8bc9776387b6a4410320f76376e
Pasquale D <69241933+DFFspace@users.noreply.github.com>
2023-01-20 12:19:48 +0100

    Update check_duplicates.py

--- check_duplicates.py
+++ check_duplicates.py
@@ -12,14 +12,14 @@ def check_duplicated_extensions():
 
     # When reading the line numbers make sure to add +1 as we start the count from zero
     for extension_name, line_number in indeces.items():
-        if len(line_number) >= 1:
+        if len(line_number) > 1:
             # Create a error and output the duplicates, fix them and than run again
             print(extension_name, line_number, file=sys.stderr)
-            sys.exit(59)
-        else:
+            #sys.exit(59)
+        #else:
             # Write something to trigger workflow create known extensions txt file
-            print("Calling other workflow....", file=sys.stderr)
-            sys.exit(0)
+            #print("Calling other workflow....", file=sys.stderr)
+            #sys.exit(0)
 
 
 check_duplicated_extensions()
